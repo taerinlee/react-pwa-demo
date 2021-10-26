de914a12c4395375118eeb09e6f185e36970b36f
Taerin Lee <kr95747@navercorp.com>
2021-10-26 13:13:58 +0900

    Add click event to move detail

--- src/views/List.js
+++ src/views/List.js
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const List = () => {
+    const history = useHistory();
     const [list, setList] = useState([]);
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => setList(json))
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => setList(json))
+    }, [])
+
+    const handleClickMove = (e, { id }) => {
+        history.push(`/${id}`);
+    }
 
     return (
         <ul>
             {list?.map((item, idx) => (
-                <li key={`list=${idx}`}>{item.name}</li>
+                <li key={`list=${idx}`} onClick={(e) => handleClickMove(e, { id: item.id })}>{item.name}</li>
             ))}
         </ul>
     )
