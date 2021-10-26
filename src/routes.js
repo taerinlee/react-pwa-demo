349e63bfd54eac4168b3b06b0b3742fa5fb53f73
Taerin Lee <kr95747@navercorp.com>
2021-10-26 13:18:51 +0900

    Add detail page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
 
 import List from "./views/List";
+import Detail from "./views/Detail";
 
 const CustomRoute = () => (
   <Switch>
     <Route exact path="/" component={List}/>
+    <Route exact path="/:id" component={Detail}/>
   </Switch>
 )
 
--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
+
 const Detail = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(response => response.json())
+        .then(json => setUser(json))
+    }, [id])
+
     return (
-        <div>Detail</div>
+        <div>name: {user?.name}</div>
     )
-}
+};
 
 export default Detail;
