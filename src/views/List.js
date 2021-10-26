39ac26127b84c8e10f835bf019116f7083610d36
Taerin Lee <kr95747@navercorp.com>
2021-10-26 13:09:34 +0900

    Add example list

--- src/views/List.js
+++ src/views/List.js
@@ -1,5 +1,19 @@
-const List = () => (
-    <div>List</div>
-);
+import { useState } from "react";
+
+const List = () => {
+    const [list, setList] = useState([]);
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => setList(json))
+
+    return (
+        <ul>
+            {list?.map((item, idx) => (
+                <li key={`list=${idx}`}>{item.name}</li>
+            ))}
+        </ul>
+    )
+};
 
 export default List;
