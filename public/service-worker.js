be54cf38861aa771fdb148e9d137367f8c5d97d2
pj.taerin.lee90@kalmate.net <pj.taerin.lee90@kalmate.net>
2021-10-29 10:28:50 +0900

    Add comment

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'my-site-cache-v3';
 var urlsToCache = [
   '/',
 ];
@@ -12,20 +12,26 @@ self.addEventListener('install', function(event) {
       );
   });
 
+  // 호출한 url을 캐시에 저장
   self.addEventListener('fetch', function(event) {
     event.respondWith(
+      // caches: 캐시스토리지안에 데이터
+      // event.request: 호출된 url주소
       caches.match(event.request)
         .then(function(response) {
+          // 현재 주소와 같은 url이 있는 경우 바로 리턴
           if (response) {
             return response;
           }
           return fetch(event.request).then(
             function(response) {
   
+              // 현재 호출된 주소의 결과를 복사
               var responseToCache = response.clone();
   
               caches.open(CACHE_NAME)
                 .then(function(cache) {
+                  // 캐시 스토리지에 저장
                   cache.put(event.request, responseToCache);
                 });
   
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,5 @@
-export function register(config) {
+// 서비스 워커를 등록하기위해 로드
+export function register() {
   if ('serviceWorker' in navigator) {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
@@ -8,18 +9,20 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      registerValidSW(swUrl, config);
+      registerValidSW(swUrl);
     });
   } else {
     console.log("won't register serviceWorker. Current env:", process.env.NODE_ENV);
   }
 }
 
+// swUrl이 유효 하다면 서비스 워커 등록을 위한 함수
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
       registration.onupdatefound = () => {
+        // 서비스워커 인스톨 유무 체크
         const installingWorker = registration.installing;
         if (installingWorker == null) {
           return;
@@ -44,6 +47,7 @@ function registerValidSW(swUrl, config) {
     });
 }
 
+// index.js에서 호출하면 서비스 워커 등록 해제 됨
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
